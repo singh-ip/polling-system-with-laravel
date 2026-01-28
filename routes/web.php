8377efc19375bea97d6dcb333d8b6881bc6fff99
singh-ip <inderpreetnarula@yahoo.com>
2026-01-28 11:40:53 +0530

    Polls web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PollController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/polls/{poll}', [PollController::class, 'show'])->name('polls.show');
+Route::post('/polls/{poll}/vote', [PollController::class, 'vote'])->name('polls.vote');
+Route::get('/polls', [PollController::class, 'index'])->name('polls.index');
